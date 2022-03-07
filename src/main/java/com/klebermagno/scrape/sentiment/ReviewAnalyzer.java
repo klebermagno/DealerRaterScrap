3fe21ecc4c406e257a5d0c1103070aa741a11091
Kleber Magno <klebermagno@gmail.com>
2022-03-07 14:15:27 -0300

    Fix mistyping

--- src/main/java/com/klebermagno/scrape/sentiment/ReviewAnalyzer.java
+++ src/main/java/com/klebermagno/scrape/sentiment/ReviewAnalyzer.java
@@ -11,23 +11,27 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ReviewAnalyzer {
 
-
+    private double scoreCutLine = 300;
 
     public  List<ReviewSentiment> analyze(List<DealerRaterReview> reviews) {
-        log.info("Analyze review sentiment: ");
+        log.info("Analyze " +reviews.size()+" review sentiment. ");
         SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
         sentimentAnalyzer.initialize();
-        List<ReviewSentiment> reviewSentmentList = new ArrayList<>();
+        List<ReviewSentiment> reviewSentimentList = new ArrayList<>();
         for (DealerRaterReview review : reviews) {
             ReviewSentiment rs = new ReviewSentiment();
             rs.setDealerRaterReview(review);
-            reviewSentmentList.add(rs);
+            reviewSentimentList.add(rs);
             rs.setTitlePositiveScore(analyzeTextSentiment(rs,sentimentAnalyzer));
             analyzeTotalScore(rs);
         }
+        return descendingSort(reviewSentimentList);
+    }
 
-        return descendingSort(reviewSentmentList);
-
+    public List<ReviewSentiment> removeCutLine(List<ReviewSentiment> descendingSort) {
+        ArrayList<ReviewSentiment> descendingSortLineCut = new ArrayList<>(descendingSort);
+        descendingSortLineCut.removeIf(reviewSentiment -> reviewSentiment.getTotalScore()<this.scoreCutLine);
+        return descendingSortLineCut;
     }
 
     private List<ReviewSentiment> descendingSort(List<ReviewSentiment> reviewSentmentList) {
@@ -44,14 +48,11 @@ public class ReviewAnalyzer {
        int qw =rs.getDealerRaterReview().getQualityOfWork();
        int ra =rs.getDealerRaterReview().getRatingStatic();
        int oa = rs.getDealerRaterReview().getOverallExperience();
-       rs.setTotalScore(cs+fl+pr+qw+ra+oa+wholeScore*10);
-
-
-
+       int rc = rs.getDealerRaterReview().getRecommendDealer()?1:-1;
+       rs.setTotalScore((cs+fl+pr+qw+ra+oa+wholeScore*10)*rc);
     }
 
     private double analyzeTextSentiment(ReviewSentiment rs, SentimentAnalyzer sentimentAnalyzer) {
-
         SentimentResult sentimentResult = sentimentAnalyzer.getSentimentResult(
                 rs.getDealerRaterReview().getTitle() + " " + rs.getDealerRaterReview().getReview());
         log.debug(sentimentResult.toString());
