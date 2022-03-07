9004a587b4a90145d4758dd6fbebf253c645a615
Kleber Magno <klebermagno@gmail.com>
2022-03-07 13:55:14 -0300

    Add fake object to test.

--- src/test/java/com/klebermagno/scrape/sentiment/ObjectDealerRaterReviews.java
+++ src/test/java/com/klebermagno/scrape/sentiment/ObjectDealerRaterReviews.java
@@ -10,41 +10,67 @@ public class ObjectDealerRaterReviews {
     public static  List<DealerRaterReview>  objectDealerRaterReviews(){
         List<DealerRaterReview> dealerRaterReviews = new ArrayList<>();
         DealerRaterReview dealerRaterReview = DealerRaterReview.builder()
-                .title("I came in for an appointment Thursday morning for a").
-                review(" transmission flush service. Mariela Hernandez got my truck checked in pretty quickly. When they where all done she gave me a call to come pick it up when I had time.  Overall great job she did at getting me in and great job on the transmission flush I can tell that it made a big difference bc it shifts an runs a lot smoother than previously.  Thanks you guys again for taking care of me and my vehicle.").
-                user("by Richard Irvine").
+                .title("Always have a great experience been going there for 6").
+                review(" years and will always go back great service great attitude when you go in .").
+                user("by johnkim2005webb").
                 ratingStatic(50).
                 customerService(50).
                 friendliness(50).
                 overallExperience(50).
                 qualityOfWork(50).
                 pricing(50).
+                recommendDealer(true).
+                build();
+        dealerRaterReviews.add(dealerRaterReview);
+        dealerRaterReview = DealerRaterReview.builder()
+                .title("Came from Longview to look at a new car. Jeannie was very").
+                review(" bad polite and effective. She was able to get me exactly what I needed. ").
+                user("by laurazieglertwins").
+                ratingStatic(10).
+                customerService(20).
+                friendliness(30).
+                overallExperience(10).
+                qualityOfWork(20).
+                pricing(30).
                 recommendDealer(false).
                 build();
         dealerRaterReviews.add(dealerRaterReview);
         dealerRaterReview = DealerRaterReview.builder()
-                .title("I came in for an appointment Thursday morning for a").
-                review(" transmission flush service. Mariela Hernandez got my truck checked in pretty quickly. When they where all done she gave me a call to come pick it up when I had time.  Overall great job she did at getting me in and great job on the transmission flush I can tell that it made a big difference bc it shifts an runs a lot smoother than previously.  Thanks you guys again for taking care of me and my vehicle.").
-                user("by Richard Irvine").
+                .title("Very helpful made sure I got a newer truck Brandon was").
+                review(" great with getting the deal done thanks to everyone for help super nice guys can’t wait to buy my first new vehicle next!!!!").
+                user("by Bobbyphillips1981").
                 ratingStatic(50).
                 customerService(50).
                 friendliness(50).
                 overallExperience(50).
                 qualityOfWork(50).
                 pricing(50).
-                recommendDealer(false).
+                recommendDealer(true).
                 build();
         dealerRaterReviews.add(dealerRaterReview);
         dealerRaterReview = DealerRaterReview.builder()
-                .title("I came in for an appointment Thursday morning for a").
-                review(" transmission flush service. Mariela Hernandez got my truck checked in pretty quickly. When they where all done she gave me a call to come pick it up when I had time.  Overall great job she did at getting me in and great job on the transmission flush I can tell that it made a big difference bc it shifts an runs a lot smoother than previously.  Thanks you guys again for taking care of me and my vehicle.").
-                user("by Richard Irvine").
+                .title("Extreme helpful!!").
+                review(" great Best Best!!!!").
+                user("by Johny Walker").
                 ratingStatic(50).
                 customerService(50).
                 friendliness(50).
                 overallExperience(50).
                 qualityOfWork(50).
                 pricing(50).
+                recommendDealer(true).
+                build();
+        dealerRaterReviews.add(dealerRaterReview);
+        dealerRaterReview = DealerRaterReview.builder()
+                .title("Extreme bad!!").
+                review(" Poor!!!").
+                user("by Nobody").
+                ratingStatic(10).
+                customerService(50).
+                friendliness(20).
+                overallExperience(20).
+                qualityOfWork(30).
+                pricing(30).
                 recommendDealer(false).
                 build();
         dealerRaterReviews.add(dealerRaterReview);
