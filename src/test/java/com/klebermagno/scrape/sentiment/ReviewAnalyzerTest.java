ef3b8c34a405ab1af54379533020b2ffbe259498
Kleber Magno <klebermagno@gmail.com>
2022-03-07 11:51:20 -0300

    Add some tesst

---
+++ src/test/java/com/klebermagno/scrape/sentiment/ReviewAnalyzerTest.java
@@ -0,0 +1,38 @@
+package com.klebermagno.scrape.sentiment;
+
+import com.klebermagno.scrape.model.DealerRaterReview;
+import com.klebermagno.scrape.model.ReviewSentiment;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.klebermagno.scrape.sentiment.ObjectDealerRaterReviews.objectDealerRaterReviews;
+import static org.junit.Assert.assertEquals;
+
+public class ReviewAnalyzerTest {
+
+
+    @Test
+    public void shouldAnalyze() {
+        List<DealerRaterReview> dealerRaterReviews = objectDealerRaterReviews();
+        ReviewAnalyzer analyzer = new ReviewAnalyzer();
+        List<ReviewSentiment> reviewSentiments = analyzer.analyze(dealerRaterReviews);
+        assertEquals(340.0, reviewSentiments.get(0).getTotalScore(), 0);
+    }
+
+    @Test
+    public void shouldDontRecommend() {
+        List<DealerRaterReview> dealerRaterReviews = objectDealerRaterReviews();
+        ReviewAnalyzer analyzer = new ReviewAnalyzer();
+        List<ReviewSentiment> reviewSentiments = analyzer.analyze(dealerRaterReviews);
+        assertEquals(-150.0, reviewSentiments.get(3).getTotalScore(), 0);
+    }
+
+    @Test
+    public void shouldSort() {
+        List<DealerRaterReview> dealerRaterReviews = objectDealerRaterReviews();
+        ReviewAnalyzer analyzer = new ReviewAnalyzer();
+        List<ReviewSentiment> reviewSentiments = analyzer.analyze(dealerRaterReviews);
+        assertEquals(330.0, reviewSentiments.get(0).getTotalScore(), 0);
+    }
+}
